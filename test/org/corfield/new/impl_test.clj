34bb6b4c11716087857b365a81d90266b539aa73
Sean Corfield <sean@corfield.org>
2021-08-09 18:34:52 -0700

    add more tests

--- test/org/corfield/new/impl_test.clj
+++ test/org/corfield/new/impl_test.clj
@@ -1,7 +1,8 @@
 ;; copyright (c) 2021 sean corfield, all rights reserved
 
 (ns org.corfield.new.impl-test
-  (:require [expectations.clojure.test :refer [defexpect expect]]
+  (:require [expectations.clojure.test
+             :refer [defexpect expect expecting from-each more more-of]]
             [org.corfield.new.impl :as sut]))
 
 (defexpect test->ns
@@ -12,8 +13,25 @@
   (expect "org/corfield/new/impl_test"
           (#'sut/->file "org.corfield.new.impl-test")))
 
-;; sut/find-root
-;; sut/->subst-map
+(defexpect test-find-root
+  (expecting "existing templates"
+             (expect some? (sut/find-root 'org.corfield.new/app))
+             (expect some? (sut/find-root 'org.corfield.new/app)))
+  (expecting "missing templates"
+             (expect nil?  (sut/find-root 'org.corfield.new/no-such-template))))
+
+(defexpect test->subst-map
+  (expect (more (comp string? key)
+                (comp string? val)
+                #(re-find #"^\{\{.*\}\}" (key %)))
+          (from-each [kv (sut/->subst-map {:a 42 :b "bee" :c true})]
+                     kv))
+  (expect (more-of {a "{{a}}" b "{{b}}" c "{{c}}"}
+                   "42"   a
+                   "bee"  b
+                   "true" c)
+          (sut/->subst-map {:a 42 :b "bee" :c true})))
+
 ;; #'sut/substitute
 ;; sut/copy-template-dir
 ;; #'sut/deconstruct-project-name
